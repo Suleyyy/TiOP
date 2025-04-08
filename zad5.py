869eb20c80a9ec0af7a925500f33f3599cd4073a
admin . <mikolajsulkowski675@gmail.com>
2025-04-08 18:57:47 +0200

    split

--- zad5.py
+++ zad5.py
@@ -5,10 +5,10 @@ img = cv2.imread("img/auto.jpg")
 
 mask = np.zeros(img.shape[:2], np.uint8)
 cv2.rectangle(mask, (90, 180), (460, 470), 255, -1)
-masked_img = cv2.bitwise_and(img, img, mask=mask)
+
 (B, G, R) = cv2.split(img)
-R = cv2.add(B, 50)
-merge = cv2.merge((B, G, R))
-new = cv2.bitwise_and(img,merge, mask=mask)
-cv2.imshow("img", new)
+R = np.where(mask == 255, np.clip(R + 50, 0, 255), R)
+merged = cv2.merge((B, G, R))
+
+cv2.imshow("img", merged)
 cv2.waitKey(0)
