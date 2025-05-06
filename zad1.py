a352370670b3037229ba9172cc3dba5be92d1d4b
admin . <mikolajsulkowski675@gmail.com>
2025-05-06 14:51:19 +0200

    zad1

---
+++ zad1.py
@@ -0,0 +1,26 @@
+import cv2
+
+img = cv2.imread('img/obraz.jpg')
+ksize = [(3,3),(5,5),(7,7)]
+i = 1
+
+for kX, kY in ksize:
+    blur = cv2.blur(img,(kX,kY))
+    cv2.imshow(f'blur{i}', blur)
+    gblur = cv2.GaussianBlur(img, (kX, kY), 0)
+    cv2.imshow(f'gblur{i}', gblur)
+    mblur = cv2.medianBlur(img, kX)
+    cv2.imshow(f'mblur{i}', mblur)
+    i += 1
+    cv2.waitKey(0)
+
+params = [(11,21,7),(11,41,21),(11,61,39)]
+
+for (diameter,sigmaColor,SigmaSpace) in params:
+    bfilter = cv2.bilateralFilter(img, diameter, sigmaColor, SigmaSpace)
+    cv2.imshow(f'bfilter{i}', bfilter)
+    i +=1
+
+cv2.waitKey(0)
+
+
